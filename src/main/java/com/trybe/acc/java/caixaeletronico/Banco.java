e1c7b72e687928248a3b11e3173316d2df23be91
WBomfim <willianbomfim@hotmail.com>
2023-02-26 07:24:23 -0300

    test/feat: Implementado a classe Banco e seus testes com cobertura 100%

--- src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Banco.java
@@ -1,5 +1,85 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Class Banco.
+ *
+ */
 public class Banco {
+  private ArrayList<PessoaCliente> clientes;
+  private ArrayList<Conta> contas;
+
+  public Banco() {
+    clientes = new ArrayList<PessoaCliente>();
+    contas = new ArrayList<Conta>();
+  }
+
+  /**
+   * Method gerarNumeroNovaConta.
+   */
+  public String gerarNumeroNovaConta() {
+    String numeroConta = "";
+    Random random = new Random();
+
+    for (int i = 0; i < 10; i++) {
+      int digito = random.nextInt(10);
+      numeroConta += Integer.toString(digito);
+    }
+
+    return numeroConta;
+  }
+
+  /**
+   * Method adicionarPessoaCliente.
+   */
+  public PessoaCliente adicionarPessoaCliente(String nome, String cpf, String senha) {
+    PessoaCliente cliente = new PessoaCliente(nome, cpf, senha);
+    clientes.add(cliente);
+    return cliente;
+  }
+
+  public void adicionarConta(Conta conta) {
+    contas.add(conta);
+  }
+
+  public ArrayList<Conta> getContas() {
+    return contas;
+  }
+
+  /**
+   * Method pessoaClienteLogin.
+   */
+  public PessoaCliente pessoaClienteLogin(String cpf, String senha) {
+    for (PessoaCliente cliente : clientes) {
+      if (cliente.getCpf().equals(cpf) && cliente.validarSenha(senha)) {
+        return cliente;
+      }
+    }
+
+    return null;
+  }
+
+  public void transferirFundos(
+      PessoaCliente pessoaCliente,
+      int daConta,
+      int paraConta,
+      double quantia) {
+    this.sacar(pessoaCliente, daConta, quantia);
+    this.depositar(pessoaCliente, paraConta, quantia);
+  }
+
+  public void sacar(PessoaCliente pessoaCliente, int daConta, double quantia) {
+    pessoaCliente.adicionarTransacaoContaEspecifica(daConta, quantia, "Saque");
+  }
+
+  public void depositar(PessoaCliente pessoaCliente, int paraConta, double quantia) {
+    pessoaCliente.adicionarTransacaoContaEspecifica(paraConta, quantia, "Depósito");
+  }
+
+  public void mostrarExtrato(PessoaCliente pessoaCliente, int conta) {
+    pessoaCliente.retornarExtratoContaEspecifica(conta);
+  }
 
 }
--- src/test/java/com/trybe/acc/java/caixaeletronico/BancoTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/BancoTest.java
@@ -1,44 +1,98 @@
 package com.trybe.acc.java.caixaeletronico;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.regex.Pattern;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Testes para a classe Banco")
 class BancoTest {
+  private Banco banco;
+  private PessoaCliente cliente;
+
+  public BancoTest() {
+    banco = new Banco();
+    cliente = banco.adicionarPessoaCliente("Alexiania Pereira", "842.074.410-77", "1234");
+  }
 
   @Test
   @DisplayName("1 - Testa o gerador de número único para nova conta.")
   void gerarNumeroNovaContaTest() {
-    fail("Não implementado");
+    String numeroConta = banco.gerarNumeroNovaConta();
+    boolean numeroContaValido = Pattern.matches("\\d{10}", numeroConta);
+    assertTrue(numeroContaValido);
   }
 
   @Test
   @DisplayName("2 - Testa o método adicionar pessoa cliente retorna o objeto pessoa cliente.")
   void adicionarPessoaClienteTest() {
-    fail("Não implementado");
+    assertNotNull(cliente);
+    assertEquals("842.074.410-77", cliente.getCpf());
+  }
 
+  @Test
+  @DisplayName("2.1 - Testa o método adicionar conta.")
+  void adicionarContaTest() {
+    Conta conta = new Conta("Corrente", cliente, banco);
+    banco.adicionarConta(conta);
+    assertEquals(1, banco.getContas().size());
   }
 
   @Test
   @DisplayName("3 - Testa o método login da pessoa cliente retorna o objeto pessoa cliente corretamente.")
   void pessoaClienteLoginTest() {
-    fail("Não implementado");
-
+    PessoaCliente clienteLogado = banco.pessoaClienteLogin("842.074.410-77", "1234");
+    assertNotNull(clienteLogado);
+    assertEquals("842.074.410-77", clienteLogado.getCpf());
+    
+    PessoaCliente clienteInvalido = banco.pessoaClienteLogin("992.074.410-77", "1234");
+    assertNull(clienteInvalido);
   }
 
   @Test
   @DisplayName("4 - Testa se o método transferir fundos está transferindo corretamente.")
   void depositarTestTransferirFundosTestmostrarExtratoTest() {
-    fail("Não implementado");
+    PessoaCliente clienteLogado = this.banco.pessoaClienteLogin("842.074.410-77", "1234");
+    clienteLogado.adicionarConta(new Conta("Corrente", clienteLogado, this.banco));
+    clienteLogado.adicionarConta(new Conta("Poupança", clienteLogado, this.banco));
+    this.banco.depositar(clienteLogado, 0, 1000.0);
+    this.banco.transferirFundos(clienteLogado, 0, 1, 300.0);
 
+    ByteArrayOutputStream saidaConsole = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(saidaConsole));
+
+    this.banco.mostrarExtrato(clienteLogado, 0);
+    assertTrue(saidaConsole.toString().contains("Depósito"));
+    assertTrue(saidaConsole.toString().contains("1000.0"));
+    assertTrue(saidaConsole.toString().contains("Saque"));
+    assertTrue(saidaConsole.toString().contains("300.0"));
+
+    saidaConsole = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(saidaConsole));
+
+    this.banco.mostrarExtrato(clienteLogado, 1);
+    assertTrue(saidaConsole.toString().contains("Depósito"));
+    assertTrue(saidaConsole.toString().contains("300.0"));
   }
 
   @Test
   @DisplayName("5 - Testa se o método sacar está funcionando corretamente.")
   void depositarTestSacarTestMostrarExtratoTest() {
-    fail("Não implementado");
+    PessoaCliente clienteLogado = this.banco.pessoaClienteLogin("842.074.410-77", "1234");
+    clienteLogado.adicionarConta(new Conta("Corrente", clienteLogado, this.banco));
+    this.banco.depositar(clienteLogado, 0, 1000.0);
+    this.banco.sacar(clienteLogado, 0, 300.0);
 
+    ByteArrayOutputStream saidaConsole = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(saidaConsole));
+
+    this.banco.mostrarExtrato(clienteLogado, 0);
+    assertTrue(saidaConsole.toString().contains("Saque"));
+    assertTrue(saidaConsole.toString().contains("300.0"));
   }
 
 }
