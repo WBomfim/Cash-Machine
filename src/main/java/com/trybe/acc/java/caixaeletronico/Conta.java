d11b9afc5584dcb0540f1b86d82e7ea6e2b68351
WBomfim <willianbomfim@hotmail.com>
2023-02-26 07:21:37 -0300

    test/feat: Implementado a classe Conta e seus testes com cobertura 100%

--- src/main/java/com/trybe/acc/java/caixaeletronico/Conta.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Conta.java
@@ -1,6 +1,73 @@
 package com.trybe.acc.java.caixaeletronico;
 
-public class Conta {
+import java.util.ArrayList;
 
+/**
+ * Class Conta.
+ *
+ */
+public class Conta {
+  private String tipoConta;
+  private String idConta;
+  private PessoaCliente pessoaCliente;
+  private ArrayList<Transacao> transacoes;
+
+  /**
+   * Contructor Class Conta.
+   */
+  public Conta(String tipoConta, PessoaCliente pessoaCliente, Banco banco) {
+    this.tipoConta = tipoConta;
+    this.idConta = banco.gerarNumeroNovaConta();
+    this.pessoaCliente = pessoaCliente;
+    this.transacoes = new ArrayList<Transacao>();
+  }
+
+  public String getTipoConta() {
+    return this.tipoConta;
+  }
+
+  public String getIdConta() {
+    return this.idConta;
+  }
+
+  public PessoaCliente getPessoaCliente() {
+    return this.pessoaCliente;
+  }
+
+  public void adicionarTransacao(double quantia, String descricao) {
+    Transacao novaTransacao = new Transacao(quantia, descricao);
+    this.transacoes.add(novaTransacao);
+  }
+
+  /**
+   * Method retornarSaldo.
+   */
+  public double retornarSaldo() {
+    double saldo = 0;
+    for (Transacao transacao : this.transacoes) {
+      if (transacao.getDescricao() == "Depósito") {
+        saldo += transacao.getQuantia();
+      }
+
+      if (transacao.getDescricao() == "Saque") {
+        saldo -= transacao.getQuantia();
+      }
+    }
+    return saldo;
+  }
+
+  public String retornarResumoConta() {
+    return "Conta: " + this.tipoConta + " " + this.idConta + " com saldo de: "
+        + this.retornarSaldo();
+  }
+
+  /**
+   * Method retornarExtrato.
+   */
+  public void retornarExtrato() {
+    for (Transacao transacao : this.transacoes) {
+      System.out.println(transacao.retornarResumoTransacao());
+    }
+  }
 
 }
--- src/test/java/com/trybe/acc/java/caixaeletronico/ContaTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/ContaTest.java
@@ -1,50 +1,84 @@
 package com.trybe.acc.java.caixaeletronico;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.regex.Pattern;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Teste da classe Conta")
 class ContaTest {
+  private String tipoConta;
+  private Conta conta;
+  private PessoaCliente pessoaCliente;
+  private Banco banco;
+
+  public ContaTest() {
+    this.tipoConta = "Corrente";
+    this.pessoaCliente = new PessoaCliente("Alexiania Pereira", "842.074.410-77", "1234");
+    this.banco = new Banco();
+    this.conta = new Conta(tipoConta, pessoaCliente, banco);
+    conta.adicionarTransacao(100.0, "Depósito");
+    conta.adicionarTransacao(50.0, "Depósito");
+    conta.adicionarTransacao(30.0, "Saque");
+  }
+
 
   @Test
   @DisplayName("6 - Testa o construtor da classe conta.")
   void construtorTest() {
-    fail("Não implementado");
+    assertEquals(tipoConta, conta.getTipoConta());
+    assertEquals(pessoaCliente, conta.getPessoaCliente());
   }
 
   @Test
   @DisplayName("7 - Testa o método adicionar transação e retornar saldo da conta.")
   void adicionarTransacaoTestRetornarSaldoTest() {
-    fail("Não implementado");
-
+    assertEquals(120.0, conta.retornarSaldo());
   }
 
   @Test
   @DisplayName("8 - Testa o método retornar resumo está retornando uma string com os valores corretamente.")
   void retornarResumoContaTest() {
-    fail("Não implementado");
-
+    String idConta = conta.getIdConta();
+    String tipoConta = conta.getTipoConta();
+    String resumoConta = conta.retornarResumoConta();
+    String resumoEsperado = "Conta: " + tipoConta + " " + idConta + " com saldo de: " + conta.retornarSaldo();
+    assertEquals(resumoEsperado, resumoConta);
   }
 
   @Test
   @DisplayName("9 - Testa o método retornar extrato está imprimindo os valores corretamente.")
   void retornarExtratoTest() {
-    fail("Não implementado");
+    ByteArrayOutputStream saidaConsole = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(saidaConsole));
 
+    conta.retornarExtrato();
+    String saida = saidaConsole.toString();
+    
+    assertTrue(saida.contains("Depósito"));
+    assertTrue(saida.contains("100.0"));
+    assertTrue(saida.contains("50.0"));
+    assertTrue(saida.contains("Saque"));
+    assertTrue(saida.contains("30.0"));
   }
 
   @Test
   @DisplayName("10 - Testa o método Getter do atributo idConta está retornando.")
   void getIdContaTest() {
-    fail("Não implementado");
-
+    String idConta = conta.getIdConta();
+    boolean idContaValido = Pattern.matches("\\d{10}", idConta);
+    assertTrue(idContaValido);
   }
 
   @Test
   @DisplayName("11 - Testa o método método Getter do atributo pessoaCliente está retornando.")
   void getPessoaClienteTest() {
-    fail("Não implementado");
+    PessoaCliente pessoaCliente = conta.getPessoaCliente();
+    assertEquals(this.pessoaCliente, pessoaCliente);
   }
 
 }
