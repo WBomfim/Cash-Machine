e4ca905ccbe831c55356de300a96fd33b5c27006
WBomfim <willianbomfim@hotmail.com>
2023-02-26 07:15:28 -0300

    test/feat: Implementado a classe Transacao e seus testes com cobertura 100%

--- src/main/java/com/trybe/acc/java/caixaeletronico/Transacao.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/Transacao.java
@@ -1,6 +1,45 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Class Trasacao.
+ *
+ */
 public class Transacao {
+  private double quantia;
+  private String instante;
+  private String descricao;
+
+  /**
+   * Contructor Class Transacao.
+   */
+  public Transacao(double quantia, String descricao) {
+    this.quantia = quantia;
+    this.instante = retornarInstante();
+    this.descricao = descricao;
+  }
+
+  public double getQuantia() {
+    return this.quantia;
+  }
+
+  public String getDescricao() {
+    return this.descricao;
+  }
+
+  public String retornarResumoTransacao() {
+    return this.descricao + " de " + this.quantia + " em " + this.instante;
+  }
+
+  /**
+   * Method retornarInstante.
+   */
+  public String retornarInstante() {
+    LocalDateTime agora = LocalDateTime.now();
+    DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    return agora.format(formato);
+  }
 
 }
-
--- src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
+++ src/test/java/com/trybe/acc/java/caixaeletronico/TransacaoTest.java
@@ -1,40 +1,65 @@
 package com.trybe.acc.java.caixaeletronico;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Testes dos métodos da classe Transacao")
 class TransacaoTest {
+  private double quantia;
+  private String descricao;
+  private Transacao transacao;
+  private String instante;
 
+  public TransacaoTest() {
+    this.quantia = 100.0;
+    this.descricao = "Depósito";
+    this.transacao = new Transacao(quantia, descricao);
+    this.instante = getInstante();
+  }
+
+  private String getInstante() {
+    LocalDateTime agora = LocalDateTime.now();
+    DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    return agora.format(formato);
+  }
 
   @Test
   @DisplayName("21 - Testa o método construtor da classe Transacao.")
   void construtorTest() {
-    fail("Não implementado");
-
+    assertEquals(this.quantia, transacao.getQuantia());
+    assertEquals(this.descricao, transacao.getDescricao());
   }
-
 
   @Test
   @DisplayName("22 - Testa o método Getter do atributo quantia.")
   void getQuantiaTest() {
-    fail("Não implementado");
+    assertEquals(this.quantia, transacao.getQuantia());
+  }
 
+  @Test
+  @DisplayName("22.1 - Testa o método Getter do atributo descricao.")
+  void getdescricaoTest() {
+    assertEquals(this.descricao, transacao.getDescricao());
   }
 
   @Test
   @DisplayName("23 - Testa o método retornar resumo transacao.")
   void retornarResumoTransacaoTest() {
-    fail("Não implementado");
-
+    String resumoTransacao = transacao.retornarResumoTransacao();
+    assertEquals(this.descricao + " de " + this.quantia + " em " + this.instante, resumoTransacao);
   }
 
   @Test
   @DisplayName("24 - Testa o método instante está gerando o instante corretamente.")
   void retornarInstanteTest() {
-    fail("Não implementado");
-
+    String instante = transacao.retornarInstante();
+    String instanteEsperado = getInstante();
+    assertEquals(instanteEsperado, instante);
   }
 
 }
