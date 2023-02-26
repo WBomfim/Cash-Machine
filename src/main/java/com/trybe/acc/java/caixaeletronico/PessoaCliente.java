67dc6331fb71ce92b3996a5a26890f102a9f0ab6
WBomfim <willianbomfim@hotmail.com>
2023-02-26 07:19:37 -0300

    test/feat: Implementado a classe PessoaCliente e seus testes com cobertura 100%

--- src/main/java/com/trybe/acc/java/caixaeletronico/PessoaCliente.java
+++ src/main/java/com/trybe/acc/java/caixaeletronico/PessoaCliente.java
@@ -1,5 +1,67 @@
 package com.trybe.acc.java.caixaeletronico;
 
+import java.util.ArrayList;
+
+/**
+ * Class PessoaCliente.
+ *
+ */
 public class PessoaCliente {
+  private String nome;
+  private String cpf;
+  private String senha;
+  private ArrayList<Conta> contas;
+
+  /**
+   * Constructor Class PessoaCliente.
+   */
+  public PessoaCliente(String nome, String cpf, String senha) {
+    this.nome = nome;
+    this.cpf = cpf;
+    this.senha = senha;
+    this.contas = new ArrayList<Conta>();
+    System.out.println("Nova pessoa cliente " + this.nome + " com CPF: " + this.cpf + " criada!");
+  }
+
+  public String getCpf() {
+    return this.cpf;
+  }
+
+  public void adicionarConta(Conta conta) {
+    this.contas.add(conta);
+  }
+
+  public int retornarNumeroDeContas() {
+    return this.contas.size();
+  }
+
+  public String retornarIdContaEspecifica(int indiceConta) {
+    return this.contas.get(indiceConta).getIdConta();
+  }
+
+  public double retornarSaldoContaEspecifica(int indiceConta) {
+    return this.contas.get(indiceConta).retornarSaldo();
+  }
+
+  public void retornarExtratoContaEspecifica(int indiceConta) {
+    this.contas.get(indiceConta).retornarExtrato();
+  }
+
+  public void adicionarTransacaoContaEspecifica(int indiceConta, double quantia, String descricao) {
+    this.contas.get(indiceConta).adicionarTransacao(quantia, descricao);
+  }
+
+  public boolean validarSenha(String senha) {
+    return this.senha.equals(senha);
+  }
+
+  /**
+   * Method retornarResumoContas.
+   */
+  public void retornarResumoContas() {
+    for (Conta conta : this.contas) {
+      System.out.println(conta.retornarResumoConta());
+    }
+  }
 
 }
